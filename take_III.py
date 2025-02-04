435b4e6342773f2e265560fa7f5c8650633ea0c2
Oren <zbenmo@gmail.com>
2025-02-04 19:44:15 +0100

    casteling

--- take_III.py
+++ take_III.py
@@ -19,9 +19,10 @@ PieceStr = str # ex. 'P'
 Move = str # ex. 'e2e4' (UCI)
 
 
-default_fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+# default_fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 # default_fen = 'rnbqkbnr/ppp1pppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1'
 # default_fen = 'rnbqkbnr/ppppppPp/8/8/8/8/PPPPP1PP/RNBQKBNR w KQkq - 0 1'
+default_fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQK2R w KQkq - 0 1'
 
 
 @dataclass
@@ -99,9 +100,6 @@ class Game:
                 continue
             yield piece_str, position
 
-    def valid(self) -> bool:
-        return True # TODO:
-
 
 @dataclass(repr=False)
 class Square():
@@ -123,7 +121,7 @@ class Square():
     def col(self) -> str:
         return self.position[0]
 
-    def add(self, rows: int, cols: int) -> Position | None:
+    def add(self, rows: int=0, cols: int=0) -> Position | None:
         row = self._row + rows
         col = self._col + cols
         if row < 0 or row > 7:
@@ -136,23 +134,50 @@ class Square():
         return self.position
 
 
+IsChecked = Callable[[Game], bool]
+
+
 class Piece(ABC):
     def __init__(self, piece_str: PieceStr, position: Position):
         self._piece_str = piece_str
         self._square = Square(position)
 
     @abstractmethod
-    def possible_moves(self) -> Generator[Tuple[Move, Game],None,None]:
+    def possible_moves(self, is_checked: IsChecked) -> Generator[Tuple[Move, Game],None,None]:
+        pass
+
+    @abstractmethod
+    def positions_threats(self, game: Game) -> Generator[Position, None, None]:
         pass
 
     def same_color(self, other_piece_str: PieceStr) -> bool:
         return self._piece_str.isupper() == other_piece_str.isupper()
 
-    def attempt_move(self, game: Game, move_square_str: Position, promotion: str = None) -> Tuple[Move, Game] | None:
+    def attempt_move(self,
+                     game: Game,
+                     move_square_str: Position,
+                     is_checked: IsChecked,
+                     promotion: str = None,
+                     extra_from_to: Tuple[Position, Position] = None,
+                     extra_remove: Position = None,
+                     ) -> Tuple[Move, Game] | None:
+        """Note: The next_game_state, that is being returned, needs some additional working by the caller
+        (board is correct yet other properties may need adjustment).
+        """
         next_game_state = deepcopy(game)
-        next_game_state.board[str(self._square)] = EMPTY
-        next_game_state.board[move_square_str] = promotion or self._piece_str
-        if not next_game_state.valid():
+        next_game_state.board[self._square.position], next_game_state.board[move_square_str] = (
+            EMPTY, (promotion or self._piece_str)
+        )
+        if extra_from_to:
+            # relevant for example for castling
+            extra_from, extra_to = extra_from_to
+            next_game_state.board[extra_from], next_game_state.board[extra_to] = (
+                EMPTY, next_game_state.board[extra_from]
+            )
+        if extra_remove:
+            # relevant for example for en-passant capture
+            next_game_state.board[extra_remove] = EMPTY
+        if is_checked(next_game_state):
             return None
         move = f'{self._square}{move_square_str}{promotion}' if promotion else f'{self._square}{move_square_str}'
         return move, next_game_state
@@ -163,13 +188,13 @@ class Piece(ABC):
         It (the logic) may be somewhat "wasteful", yet is straight-forward and is useful from various places in the code below. 
         """
         if next_game_state.board['a1'] != W_R:
-            next_game_state.castling_rights = next_game_state.replace('Q', '')
+            next_game_state.castling_rights = next_game_state.castling_rights.replace('Q', '')
         if next_game_state.board['h1'] != W_R:
-            next_game_state.castling_rights = next_game_state.replace('K', '')
+            next_game_state.castling_rights = next_game_state.castling_rights.replace('K', '')
         if next_game_state.board['a8'] != B_R:
-            next_game_state.castling_rights = next_game_state.replace('q', '')
+            next_game_state.castling_rights = next_game_state.castling_rights.replace('q', '')
         if next_game_state.board['h8'] != B_R:
-            next_game_state.castling_rights = next_game_state.replace('k', '')
+            next_game_state.castling_rights = next_game_state.castling_rights.replace('k', '')
         if next_game_state.castling_rights == '':
             next_game_state.castling_rights = '-' 
 
@@ -182,16 +207,16 @@ class Pawn(Piece):
         starting_row = ROWS[1] if self._piece_str == W_P else ROWS[-2]
         pre_promotion_row = ROWS[-2] if self._piece_str == W_P else ROWS[1]
         self.pre_promotion = pre_promotion_row == self._square.row
-        self._theoretical_moves = [self._square.add(rows=1 * self._direction, cols=0)]
+        self._theoretical_moves = [self._square.add(rows=1 * self._direction)]
         if self._square.row == starting_row:
-            self._theoretical_moves.append(self._square.add(rows=2 * self._direction, cols=0))
+            self._theoretical_moves.append(self._square.add(rows=2 * self._direction))
         self._theoretical_captures = [
             target_square_str
             for target_square_str in map(lambda x: self._square.add(rows=1 * self._direction, cols=x), [-1, +1])
             if target_square_str
         ]
 
-    def possible_moves(self, game: Game) -> Generator[Tuple[Move, Game],None,None]:
+    def possible_moves(self, game: Game, is_checked: IsChecked) -> Generator[Tuple[Move, Game],None,None]:
         if self.pre_promotion:
             promotions = [W_Q, W_R, W_B, W_N] if self._piece_str == W_P else [B_Q, B_R, B_B, B_N]
             assert len(self._theoretical_moves) < 2 # ex. white can be on row 2 or on row 7, but not on both at the same time
@@ -205,7 +230,7 @@ class Pawn(Piece):
                 if piece_there_str != EMPTY:
                     break
                 for promotion in promotions:
-                    ret = self.attempt_move(game, move_square_str, promotion)
+                    ret = self.attempt_move(game, move_square_str, is_checked, promotion=promotion)
                     if not ret:
                         continue
                     move, next_game_state = ret
@@ -216,7 +241,7 @@ class Pawn(Piece):
                     else:
                         assert game.turn == 'w'
                         next_game_state.turn = 'b'
-                    next_game_state.en_passant = self._square.add(1 * self._direction, 0) if two_steps else '-'
+                    next_game_state.en_passant = self._square.add(rows=1 * self._direction) if two_steps else '-'
                     next_game_state.half_moves = 0
                     yield move, next_game_state
                 two_steps = True
@@ -226,13 +251,16 @@ class Pawn(Piece):
         def _capture() -> Generator[Tuple[Move, Game],None,None]:
             for capture_square_str in self._theoretical_captures:
                 piece_there_str = game.board[capture_square_str]
+                extra_remove = None
                 if piece_there_str == EMPTY:
                     if game.en_passant != capture_square_str:
                         continue
+                    extra_remove = Square(capture_square_str).add(rows=1 * self.direction)
                 elif self.same_color(piece_there_str):
                     continue
                 for promotion in promotions:
-                    ret = self.attempt_move(game, capture_square_str, promotion)
+                    ret = self.attempt_move(
+                        game, capture_square_str, is_checked, promotion=promotion, extra_remove=extra_remove)
                     if not ret:
                         continue
                     move, next_game_state = ret
@@ -250,6 +278,9 @@ class Pawn(Piece):
 
         yield from _capture()
 
+    def positions_threats(self, game: Game) -> Generator[Position, None, None]:
+        yield from self._theoretical_captures
+
 
 class Knight(Piece):
     def __init__(self, piece_str: PieceStr, position: Position):
@@ -262,12 +293,12 @@ class Knight(Piece):
             if (position := self._square.add(rows=sign_row * add_row, cols=sign_col * add_col)) is not None
         ]
 
-    def possible_moves(self, game: Game) -> Generator[Tuple[Move, Game],None,None]:
+    def possible_moves(self, game: Game, is_checked: IsChecked) -> Generator[Tuple[Move, Game],None,None]:
         for move_square_str in self._theoretical_moves:
             piece_there_str = game.board[move_square_str]
             if piece_there_str != EMPTY and self.same_color(piece_there_str):
                 continue
-            ret = self.attempt_move(game, move_square_str)
+            ret = self.attempt_move(game, move_square_str, is_checked)
             if not ret:
                 continue
             move, next_game_state = ret
@@ -285,6 +316,9 @@ class Knight(Piece):
             next_game_state.en_passant = '-'
             yield move, next_game_state
 
+    def positions_threats(self, game: Game) -> Generator[Position, None, None]:
+        yield from self._theoretical_moves
+
 
 class RayBased(Piece, ABC):
     def __init__(self, piece_str: PieceStr, position: Position):
@@ -299,13 +333,13 @@ class RayBased(Piece, ABC):
     def get_rays(self) -> List[List[str]]:
         pass
 
-    def possible_moves(self, game: Game) -> Generator[Tuple[Move, Game],None,None]:
+    def possible_moves(self, game: Game, is_checked: IsChecked) -> Generator[Tuple[Move, Game],None,None]:
         for ray in self._rays:
             for target_square_str in ray:
                 piece_there_str = game.board[target_square_str]
                 if piece_there_str != EMPTY and self.same_color(piece_there_str):
                     break # this ray reached a piece of its own color
-                ret = self.attempt_move(game, target_square_str)
+                ret = self.attempt_move(game, target_square_str, is_checked)
                 if not ret:
                     continue
                 move, next_game_state = ret
@@ -324,6 +358,14 @@ class RayBased(Piece, ABC):
                 yield move, next_game_state
                 if capture:
                     break # this ray reached a piece (a capture)
+
+    def positions_threats(self, game: Game) -> Generator[Position, None, None]:
+        for ray in self._rays:
+            for target_square_str in ray:
+                piece_there_str = game.board[target_square_str]
+                yield target_square_str
+                if piece_there_str != EMPTY:
+                    break
 
 
 class RookRays():
@@ -388,34 +430,123 @@ class King(Piece):
             )
         ]
 
-    def possible_moves(self, game: Game) -> Generator[Tuple[Move, Game],None,None]:
-        for move_square_str in self._theoretical_moves:
-            piece_there_str = game.board[move_square_str]
-            if piece_there_str != EMPTY and self.same_color(piece_there_str):
-                continue
-            ret = self.attempt_move(game, move_square_str)
-            if not ret:
-                continue
-            move, next_game_state = ret
-            capture = piece_there_str != EMPTY
-            if self._piece_str == B_K:
-                assert game.turn == 'b'
-                next_game_state.move_number += 1
-                next_game_state.castling_rights = next_game_state.castling_rights.replace('k', '').replace('q', '')
-                next_game_state.turn = 'w'
-            else:
-                assert game.turn == 'w'
-                next_game_state.castling_rights = next_game_state.castling_rights.replace('K', '').replace('Q', '')
-                next_game_state.turn = 'b'
+    def possible_moves(self, game: Game, is_checked: IsChecked) -> Generator[Tuple[Move, Game],None,None]:
 
-            if next_game_state.castling_rights == '':
-                next_game_state.castling_rights = '-'
+        def _simple() -> Generator[Tuple[Move, Game],None,None]:
+            for move_square_str in self._theoretical_moves:
+                piece_there_str = game.board[move_square_str]
+                if piece_there_str != EMPTY and self.same_color(piece_there_str):
+                    continue
+                ret = self.attempt_move(game, move_square_str, is_checked)
+                if not ret:
+                    continue
+                move, next_game_state = ret
+                capture = piece_there_str != EMPTY
+                if self._piece_str == B_K:
+                    assert game.turn == 'b'
+                    next_game_state.move_number += 1
+                    next_game_state.castling_rights = next_game_state.castling_rights.replace('k', '').replace('q', '')
+                    next_game_state.turn = 'w'
+                else:
+                    assert game.turn == 'w'
+                    next_game_state.castling_rights = next_game_state.castling_rights.replace('K', '').replace('Q', '')
+                    next_game_state.turn = 'b'
 
-            next_game_state.half_moves = 0 if capture else next_game_state.half_moves + 1
-            if capture:
-                self.update_castling_rights(next_game_state)
-            next_game_state.en_passant = '-'
-            yield move, next_game_state
+                if next_game_state.castling_rights == '':
+                    next_game_state.castling_rights = '-'
+
+                next_game_state.half_moves = 0 if capture else next_game_state.half_moves + 1
+                if capture:
+                    self.update_castling_rights(next_game_state)
+                next_game_state.en_passant = '-'
+                yield move, next_game_state
+
+        yield from _simple()
+
+        def _castling() -> Generator[Tuple[Move, Game],None,None]:
+            relevant_rights = ['K', 'Q'] if self._piece_str == W_K else ['k', 'q']
+            relevant_rights = [right for right in relevant_rights if right in game.castling_rights]
+            if len(relevant_rights) < 1:
+                return
+            checked_for_check = False
+            for right in relevant_rights:
+                match right:
+                    case 'K':
+                        assert self._square.position == 'e1'
+                        assert game.board['h1'] == W_R
+                        should_be_free = ['f1', 'g1']
+                        additional_should_be_safe = ['f1'] # 'g1'
+                        target_king = 'g1'
+                        source_rook = 'h1'
+                        target_rook = 'f1'
+                    case 'Q':
+                        assert self._square.position == 'e1'
+                        assert game.board['a1'] == W_R
+                        should_be_free = ['b1', 'c1', 'd1']
+                        additional_should_be_safe = ['d1'] # 'c1'
+                        target_king = 'c1'
+                        source_rook = 'a1'
+                        target_rook = 'd1'
+                    case 'k':
+                        assert self._square.position == 'e8'
+                        assert game.board['h8'] == B_R
+                        should_be_free = ['f8', 'g8']
+                        additional_should_be_safe = ['f8'] # 'g8'
+                        source_rook = 'h8'
+                        target_king = 'g8'
+                        target_rook = 'f8'
+                    case 'q':
+                        assert self._square.position == 'e8'
+                        assert game.board['a8'] == B_R
+                        should_be_free = ['b8', 'c8', 'd8']
+                        additional_should_be_safe = ['d8'] # 'c8'
+                        target_king = 'c8'
+                        source_rook = 'a8'
+                        target_rook = 'd8'
+                    case _:
+                        assert False, f'{right=}'
+                if any(game.board[position] != EMPTY for position in should_be_free):
+                    continue # this castling is not possible
+                if not checked_for_check:
+                    if is_checked(game):
+                        return # no castling is possible
+                    checked_for_check = True
+                checked = None
+                for position in additional_should_be_safe:
+                    my_position = self._square.position
+                    game.board[my_position], game.board[position] = EMPTY, game.board[my_position]
+                    checked = is_checked(game)
+                    # take back
+                    game.board[position], game.board[my_position] = EMPTY, game.board[position]
+                    if checked:
+                        break
+                if checked:
+                    continue # this castling is not possible
+                ret = self.attempt_move(game, target_king, is_checked, extra_from_to=(source_rook, target_rook))
+                if not ret:
+                    continue # this castling is not possible
+                move, next_game_state = ret
+                if self._piece_str == B_K:
+                    assert game.turn == 'b'
+                    next_game_state.move_number += 1
+                    next_game_state.castling_rights = next_game_state.castling_rights.replace('k', '').replace('q', '')
+                    next_game_state.turn = 'w'
+                else:
+                    assert game.turn == 'w'
+                    next_game_state.castling_rights = next_game_state.castling_rights.replace('K', '').replace('Q', '')
+                    next_game_state.turn = 'b'
+
+                if next_game_state.castling_rights == '':
+                    next_game_state.castling_rights = '-'
+
+                next_game_state.half_moves = next_game_state.half_moves + 1
+                next_game_state.en_passant = '-'
+                yield move, next_game_state
+
+        yield from _castling()
+
+    def positions_threats(self, game: Game) -> Generator[Position, None, None]:
+        yield from self._theoretical_moves
 
 
 class GameEvaluation:
@@ -444,7 +575,18 @@ class GameEvaluation:
     def possible_moves(game: Game) -> Generator[Tuple[Move, Game],None,None]:
         for piece_str, position in game.all_pieces(player=game.turn):
             piece = GameEvaluation.piece_for(piece_str, position)
-            yield from piece.possible_moves(game)
+            yield from piece.possible_moves(game, GameEvaluation.is_checked)
+
+    @staticmethod
+    def is_checked(game: Game) -> bool:
+        which_king = W_K if game.turn == 'w' else B_K
+        kings_position = next(position for position, piece_str in game.board.items() if piece_str == which_king)
+        other_player = 'b' if game.turn == 'w' else 'w'
+        for piece_str, position in game.all_pieces(player=other_player):
+            piece = GameEvaluation.piece_for(piece_str, position)
+            if any(kings_position == position for position in piece.positions_threats(game)):
+                return True
+        return False
 
 
 def main():
